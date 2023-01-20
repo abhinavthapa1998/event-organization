d3c51bf5fe227320c17ddac787db39bb0392fe6e
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-01-20 22:34:52 +0530

    createEvent Function

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -13,4 +13,29 @@ contract EventContract {
     mapping(uint256 => Event) public events;
     mapping(address => mapping(uint256 => uint256)) public tickets;
     uint256 public nextId;
+
+    function createEvent(
+        string memory name,
+        uint256 date,
+        uint256 price,
+        uint256 ticketCount
+    ) external {
+        require(
+            date > block.timestamp,
+            "You can only organize events for a future date"
+        );
+        require(
+            ticketCount > 0,
+            "You can only organize events if tickets are more than 0"
+        );
+        events[nextId] = Event(
+            msg.sender,
+            name,
+            date,
+            price,
+            ticketCount,
+            ticketCount
+        );
+        nextId++;
+    }
 }
